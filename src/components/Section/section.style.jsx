11c68a8c179a868bf2419374a51f2d846a59fa97
Luis Nava <jorgex4p@gmail.com>
2024-02-02 22:04:27 -0500

    v5

--- src/components/Section/section.style.jsx
+++ src/components/Section/section.style.jsx
@@ -1,18 +1,30 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  max-width: 300px;
+
+  max-width: 300px; 
   height: 400px;
-
   margin: 0 auto;
-  text-align: center;
-
-  border: 1px solid #efefef;
-  border-radius: 20px;
+  text-align: center;  
+  border-radius: 40px;
   box-shadow: 2px 2px 3px #EFEFEF;
 
   display: flex;
-  place-content: center;
-  flex-direction: column;
+  place-content: center;  
+  flex-direction: column; 
   gap: 30px;
+
+  box-shadow: 0 0 200px red, 
+  inset 0 0 300px #460f0f;  
+
+  h2{
+    color: #460f0f;
+    text-shadow: 0 0 10px rgba(255,255,255,0.5);
+  }
+
+  li{
+    color: #460f0f;
+    text-shadow: 0 0 8px rgba(255,255,255,0.3); 
+  }
+
 `;
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -4,14 +4,14 @@ import React from 'react';
 
 export const Statistics = ({ good, neutral, bad, total, positivePercentage}) => {
   return (
-    <div>
-      <h2>Statistics</h2>
-      <ul>
-        <li>Good: {good}</li>
-        <li>Neutral: {neutral}</li>
-        <li>Bad: {bad}</li>
-        <li>Total: {total}</li>
-        <li>Positive feedback: {positivePercentage}%</li>
+    <div className="statistics">
+      <h2 className="title">Statistics</h2>
+      <ul className="list">
+        <li className="item">Good: {good}</li>
+        <li className="item">Neutral: {neutral}</li>
+        <li className="item">Bad: {bad}</li>
+        <li className="item">Total: {total}</li>
+        <li className="item">Positive feedback: {positivePercentage}%</li>
       </ul>
     </div>
   );
